9d715134be1b75ae91ed5f289db47fb50cd0d29f
zenpoxa <zenpoxa@gmail.com>
2024-07-20 17:23:15 +0200

    Projet fini + ajout de vraie création d'un fichier JSON

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,46 @@
 use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::prelude::*;
 
 
 #[derive(Serialize, Deserialize)]
 struct Paragraph {
-    name: String
+	name : String
 }
-
 
 #[derive(Serialize, Deserialize)]
 struct Article {
-    article: String,
-    author: String,
-    paragraph: Vec<Paragraph>
+	article : String,
+	author : String,
+	paragraph : Vec<Paragraph>
 }
 
+fn main () {
 
-fn main() {
-    let article: Article = Article {
-	article: String::from("how to work with json in Rust"),
-	author: String::from("akhil"),
-	paragraph: vec![
-	    Paragraph {
-		name: String::from("first sentence")
-	    },
-	    Paragraph {
-		name: String::from("body of the article")
-	    },
-	    Paragraph {
-		name: String::from("end of paragraph")
-	    }
-	]
-    };
+	let article : Article = Article {
+		article : String::from("Mon tout nouvel article"),
+		author : String::from("TERRADE Richard"),
+		paragraph : vec![
+			Paragraph {
+				name : String::from("Premier par")
+			},
+			Paragraph {
+				name : String::from("Deuxième pafdqsfdsqfdqsr")
+			},
+			Paragraph {
+				name : String::from("Fin...")
+			}
+		]
+	};
 
-    let json = serde_json::to_string(&article).unwrap();
+	let json = serde_json::to_string(&article).unwrap();
+	println!("The json is : {}", json);
 
-    println!("the JSON is: {}", json)
+	creer_json_file(&json).unwrap();
+}
+
+fn creer_json_file(content : &String) -> std::io::Result<()> {
+    let mut file = File::create("foo.json")?;
+    file.write_all(content.as_bytes())?;
+    Ok(())
 }
